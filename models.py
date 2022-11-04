b735d887ee6a83f2bf568d25942e617ba31455c0
2pokek <alkuzmich6479@gmail.com>
2022-11-04 17:03:03 +0300

    3

--- models.py
+++ models.py
@@ -1,3 +1,11 @@
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
+
+class product(db.Model):
+    id = db.Column(db.Integer,primary_key=True)
+    title =db.Column(db.String(50),nullable=False)
+    text = db.Column(db.Text,nullable=False)
+    category = db.Column(db.String(50),nullable=False)
+    price =db.Column(db.Integer,nullable=False)
+    isActive =db.Column()
