a1f0d3cee1249afd7048b8330d74fd0f984849d8
2pokek <alkuzmich6479@gmail.com>
2022-11-04 19:35:02 +0300

    6

--- main.py
+++ main.py
@@ -1,38 +1,54 @@
-from flask import Flask,render_template
+from flask import Flask, render_template, request,redirect
 from flask_sqlalchemy import SQLAlchemy
-from models import db
+from models import db, Product
 
-
-app=Flask(__name__)
+app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///shop.sqlite3.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db.init_app(app)
+
 
 @app.route('/create')
 def create():
     db.create_all()
     return 'all tables created'
 
+
 @app.route('/')
 def main():
-    return render_template('main.html')
+    products= Product.query.order_by(Product.price).all()
+    return render_template('main.html',data=products)
+
 
 @app.route('/about')
 def about():
     return render_template('about.html')
 
+
 @app.route('/favorite')
 def favorite():
     return render_template('favorite.html')
 
-@app.route('/add')
-def add():
-    return render_template('add.html')
 
+@app.route('/add', methods=["POST", "GET"])
+def add():
+    if request.method == "POST":
+        title = request.form['title']
+        text = request.form['text']
+        category = request.form['category']
+        price = request.form['price']
+
+        product = Product(title=title, text=text, category=category, price=price)
+        try:
+            db.session.add(product)
+            db.session.commit()
+            return redirect('/')
+        except:
+            return 'Something went wrong,try again'
+    else:
+        return render_template('add.html')
 
 
 if __name__ == '__main__':
     app.run(debug=True)
-
-
--- models.py
+++ models.py
@@ -2,10 +2,13 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-class product(db.Model):
+class Product(db.Model):
     id = db.Column(db.Integer,primary_key=True)
     title =db.Column(db.String(50),nullable=False)
     text = db.Column(db.Text,nullable=False)
     category = db.Column(db.String(50),nullable=False)
     price =db.Column(db.Integer,nullable=False)
     isActive =db.Column(db.Boolean,default=True)
+
+    def __repr__(self):
+        return self.title
