77e1b8b86418638d2f6694ea25ad126cf8d15ea9
2pokek <alkuzmich6479@gmail.com>
2022-11-04 15:24:39 +0300

    1

---
+++ main.py
@@ -0,0 +1,25 @@
+from flask import Flask,render_template
+
+app=Flask(__name__)
+
+
+@app.route('/create')
+def create():
+    db.create_all()
+    return 'all tables created'
+
+@app.route('/')
+def main():
+    return render_template('main.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
